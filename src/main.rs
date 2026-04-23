5c415618ac566acdd6bb82d64d4e6fa534847d0d
prog-ops <akunbaru14102019@gmail.com>
2026-04-23 12:36:52 +0700

    feat: Add enum and struct - rmh

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,21 @@
 #![windows_subsystem = "windows"]
 
-use windows_sys::Win32::UI::WindowsAndMessaging::{MessageBoxW, MB_OK, MB_ICONINFORMATION};
+use eframe::egui;
 
-// Helper function to create null-terminated wide string
-fn to_wstring(value: &str) -> Vec<u16> {
-    use std::os::windows::ffi::OsStrExt;
-    std::ffi::OsStr::new(value).encode_wide().chain(std::iter::once(0)).collect()
+#[derive(Clone, Copy)]
+enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+struct Calculator {
+    display: String,
+    previous_value: Option<f64>,
+    current_op: Option<Op>,
+    new_input: bool,
 }
 
 fn main() {
-    let caption = to_wstring("Pesan");
-    let text = to_wstring("Ini adalah kotak Windows sederhana dengan button OK!");
-
-    unsafe {
-        MessageBoxW(
-            0,
-            text.as_ptr(),
-            caption.as_ptr(),
-            MB_OK | MB_ICONINFORMATION,
-        );
-    }
 }
