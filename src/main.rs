436d400b79ff19357f51ae74441ff3524569c87d
prog-ops <akunbaru14102019@gmail.com>
2026-04-23 12:38:55 +0700

    feat: Add Calculator functions - rmh

--- src/main.rs
+++ src/main.rs
@@ -28,5 +28,46 @@ impl Default for Calculator {
     }
 }
 
+impl Calculator {
+    fn input_digit(&mut self, digit: &str) {
+        if self.new_input {
+            self.display = String::new();
+            self.new_input = false;
+        }
+
+        if self.display == "0" && digit != "." {
+            self.display = digit.to_owned();
+        } else {
+            self.display.push_str(digit);
+        }
+    }
+
+    fn calculate(&mut self) {
+        if let (Some(prev), Some(op)) = (self.previous_value, self.current_op) {
+            if let Ok(current) = self.display.parse::<f64>() {
+                let res = match op {
+                    Op::Add => prev + current,
+                    Op::Sub => prev - current,
+                    Op::Mul => prev * current,
+                    Op::Div => if current != 0.0 { prev / current } else { f64::NAN },
+                };
+                self.display = res.to_string();
+                self.previous_value = Some(res); // allow chaining equals or further ops
+            }
+        }
+    }
+
+    fn apply_op(&mut self, op: Op) {
+        if !self.new_input {
+            self.calculate();
+        }
+        if let Ok(current) = self.display.parse::<f64>() {
+            self.previous_value = Some(current);
+            self.current_op = Some(op);
+            self.new_input = true;
+        }
+    }
+}
+
 fn main() {
 }
